677b41be6f6b643202f20d83f5e43f045e70bfe6
Leonardo Henrique Araujo da Silva <leonardo.araujo@vitallis.com.br>
2020-09-23 12:02:16 -0300

    End of the project

--- src/app/controllers/Product.js
+++ src/app/controllers/Product.js
@@ -1,7 +1,17 @@
 const knex = require('../database/index')
-const { request, response } = require('express')
 
 class Product {
+
+    show(request, response) {
+        knex('products').where('id', request.params.id)
+            .then(res => {
+                if(res.length == 0) {
+                    return response.json({ message: 'Dados não encontrados!' })
+                } else {
+                    return response.json(res)
+                }
+            })
+    }
     
     read(request, response) {
         knex('products').then(res => {
@@ -9,15 +19,45 @@ class Product {
         })
     }
 
-    create() {
+    create(request, response) {
+        const name = request.body.name
+        const amount = request.body.amount
+
+        if(name.length == 0 || amount.length == 0) {
+            return response.json({ message: 'Informe todos os dados corretamente!' })
+        }
+
+        knex('products').insert({ name, amount })
+            .then(() => response.json({ message: 'Dados guardados com sucesso!' }))
+            .catch(err => response.json({ message: err }))
 
     }
 
-    update() {
+    update(request, response) {
+        const id = request.params.id
+        const name = request.body.name
+        const amount = request.body.amount
+
+        if(name.length == 0 || amount.length == 0) {
+            return response.json({ message: 'Informe todos os dados corretamente!' })
+        }
+
+        knex('products')
+            .where('id', id)
+            .update({ name, amount })
+                .then(() => response.json({ message: 'Dados alterados com sucesso!' }))
+                .catch(err => response.json({ message: err }))
 
     }
 
-    delete() {
+    delete(request, response) {
+        const id = request.params.id
+
+        knex('products')
+            .where('id', id)
+            .del()
+                .then(() => response.json({ message: 'Dados deletados com sucesso!' }))
+                .catch(err => response.json({ message: err }))
 
     }
 
--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -1,8 +1,15 @@
 const route = require('express').Router()
+const bodyParser = require('body-parser')
+
+route.use(bodyParser.json())
 
 const Product = require('../controllers/Product')
 const product = new Product()
 
 route.get('/', product.read)
+route.get('/:id', product.show)
+route.post('/', product.create)
+route.put('/:id', product.update)
+route.delete('/:id', product.delete)
 
 module.exports = route;
